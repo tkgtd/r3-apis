f89ba23b5daad596c63d75dc5bc6192dc2c7e5c5
Валерий Юнусов <chihalovanp@gtdel.net>
2023-01-23 12:34:21 +0500

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3OTUzNw/spectral.js?branch_slug=main&token=47e77dc9-8a4c-4bb2-bd91-47514420039e";
+export default { extends: ruleset };
